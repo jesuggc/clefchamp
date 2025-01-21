619bed26d4b606df8913c6f477a7ce6084b6f230
Jesus Gonzalez <jesugo11@ucm.es>
2025-01-21 23:19:10 +0100

    working on demo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var applicationRouter = require('./routes/application');
+var playRouter = require('./routes/play');
 var app = express();
 
 const dao = require('./public/javascripts/DAO')
@@ -59,6 +60,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/application', applicationRouter);
+app.use('/play', playRouter);
 
 
 // midao.getConfiguration((err,res)=> {
---
+++ routes/play.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+const dao = require("../public/javascripts/DAO.js");
+const { error } = require('jquery');
+const midao = new dao("localhost","root","","clefchamp","3306")
+
+const passLocals = (req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+};
+
+const isLoggedIn = (req, res, next) => {
+  if (res.locals.user) return next();
+  res.redirect('/users/login');
+};
+
+const alreadyLoggedIn = (req, res, next) => {
+  if (!res.locals.user) return next();
+  res.redirect('/');
+};
+
+router.use(passLocals)
+
+router.get("/", isLoggedIn, (request,response) => {
+  response.render('home')
+})
+
+
+router.get("/pruebaNivel", (request,response) => {
+    response.render("pruebaNivel")
+})
+
+// This is probably the worst way to do this...
+function errorHandler(err) {
+  console.log("\x1b[41m%s\x1b[0m",  "-----------------------------------------------------------")
+  console.log("\x1b[41m%s\x1b[0m",  "CÓDIGO DE ERROR: ",err.code)
+  console.log("\x1b[41m%s\x1b[0m",  "MENSAJE SQL: ",err.sqlMessage)
+  console.log("\x1b[41m%s\x1b[0m",  "SQL: ",err.sql)
+  console.log("\x1b[41m%s\x1b[0m",  "-----------------------------------------------------------")
+}
+module.exports = router;
