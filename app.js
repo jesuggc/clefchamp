0c1c163138bcd4227995363285d2391bde4c799c
Jesus Gonzalez <jesugo11@ucm.es>
2025-03-10 23:00:23 +0100

    is this working? x2

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(session({
 }))
 app.use((req,res,next) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log(`IP: ${ip}`)
+  
+  console.log( new Date(Date.now()) + `IP: ${ip}`)
   next();
 });
 
